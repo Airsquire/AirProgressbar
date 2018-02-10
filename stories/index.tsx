7805ac1eb77a82760b05a75795d592b86275fd38
You Yue <youy0004123@gmail.com>
2018-02-10 16:57:18 +0100

    Fix addon problem

--- stories/index.tsx
+++ stories/index.tsx
@@ -9,7 +9,6 @@ loadingBarStory.add("default", () => {
   const percent = number("Loading percentage", 20)
   const strokeWidth = number("Stroke width", 2)
   const strokeColor = color("Stroke Color", "#2db7f5")
-  const tailWidth = number("Tail Width", 2)
   const tailColor = color("Tail Color", "#D9D9D9")
   const strokeLinecap = select(
     "Stroke linecap",
