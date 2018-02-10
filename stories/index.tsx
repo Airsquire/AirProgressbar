6239c383965d828daa38ae8c366a57d254ad9760
You Yue <youy0004123@gmail.com>
2018-02-10 16:51:28 +0100

    Add storybook and main ts file

---
+++ stories/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import { storiesOf } from "@storybook/react"
+import { withKnobs, number, select, color } from "@storybook/addon-knobs/react"
+import * as Progressbar from "../src"
+
+const loadingBarStory = storiesOf("LoadingBar", module)
+loadingBarStory.addDecorator(withKnobs)
+loadingBarStory.add("default", () => {
+  const percent = number("Loading percentage", 20)
+  const strokeWidth = number("Stroke width", 2)
+  const strokeColor = color("Stroke Color", "#2db7f5")
+  const tailWidth = number("Tail Width", 2)
+  const tailColor = color("Tail Color", "#D9D9D9")
+  const strokeLinecap = select(
+    "Stroke linecap",
+    {
+      butt: "butt",
+      round: "round",
+      square: "square",
+      inherit: "inherit"
+    },
+    "round"
+  )
+  return (
+    <Progressbar.Line
+      percent={percent}
+      strokeWidth={strokeWidth}
+      strokeColor={strokeColor}
+      tailColor={tailColor}
+      strokeLinecap={strokeLinecap}
+    />
+  )
+})
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// const webpack = require('webpack')
+// const path = require('path')
+// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+// const BUILD_DIR = path.resolve(__dirname, 'build')
+// const SRC_DIR = path.resolve(__dirname, 'src')
+
+// module.exports = {
+//   resolve: {
+//     extensions: [".ts", ".tsx", ".js",".json"],
+//     plugins: [new TsconfigPathsPlugin()]
+//   },
+//   module: {
+//     rules: [
+//       { test: /\.tsx?$/, loader: ["awesome-typescript-loader"] },
+//     ]
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//     new webpack.NamedModulesPlugin()
+//   ]
+// }
